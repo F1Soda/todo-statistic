146ecb415b764a077aaaa3ea796db8ac53a726c3
katushenca <70428604+katushenca@users.noreply.github.com>
2025-03-04 15:47:18 +0500

    add getTodoComments()

--- index.js
+++ index.js
@@ -22,4 +22,20 @@ function processCommand(command) {
     }
 }
 
-// TODO you can do it!
+function getTodoComments() {
+    const comments = [];
+    for (const file of files) {
+        const lines = file.split('\n');
+
+        for (const line of lines) {
+            line.trim(); // Убираем лишние пробелы в начале и конце
+            if (line.startsWith('// TODO ')) {
+                comments.push(line.split('\r')[0]);
+            }
+        }
+    }
+
+    return comments;
+}
+
+console.log(getTodoComments())
